e027fa70de8adadd972f0a47a31ee8297789bf01
seraphy archangel <seraphy@win8imac5.(none)>
2012-08-25 20:47:29 +0900

    MainForm

---
+++ EnumFiles/EnumFilesForm.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace EnumFiles
+{
+    public partial class EnumFilesForm : Form
+    {
+        public EnumFilesForm()
+        {
+            InitializeComponent();
+        }
+
+        private void EnumFilesForm_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void SearchConditionGroup_Enter(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
